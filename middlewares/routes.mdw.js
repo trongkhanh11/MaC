2b4caf0edcaf96d49c2ede11cc6f123b027a8771
Khanh <truongtrongkhanhcm@gmail.com>
2022-12-28 15:44:08 +0700

    update front-end

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,9 +1,12 @@
+import express from 'express';
 import accountRoute from '../routes/account.route.js';
+import videoTutorialRoute from '../routes/videoTutorial.route.js'
 
 export default function (app) {
   app.get('/', function (req, res) {
-    res.render('vwAccount/profile');
+    res.render('home');
   })
 
-  app.use('/account', accountRoute);
+  app.use('/account', accountRoute, express.static('public'));
+  app.use('/videoTutorial', videoTutorialRoute, express.static('public'));
 }
