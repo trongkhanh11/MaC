22ec04f09b654e61ec9944b7bb7d233afa8126e0
Dat <datk1912@gmail.com>
2022-12-30 14:30:44 +0700

    New Branch

---
+++ utils/db.js
@@ -0,0 +1,15 @@
+import knexObj from 'knex'
+
+const knex = knexObj({
+          client: 'mysql2',
+          connection: {
+            host : '127.0.0.1',
+            port : 3306,
+            user : 'root',
+            password : '',
+            database : 'mac'
+          },
+          pool: {min: 0, max:10}
+        });
+
+export default knex;
