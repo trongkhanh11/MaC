fc01d30b4eb3c6b16c156e2194a30482e892dec6
Dat <datk1912@gmail.com>
2023-01-02 20:06:37 +0700

    add post controller

--- services/post.model.js
+++ services/post.model.js
@@ -1,21 +1,30 @@
-//import db from '../utils/db.js'
-//
-//export default {
-//    findPost(text){
-//        return db('post').where(tenbaihat,text);
-//    },
-//
-//    getPost(id){
-//        return db('post').where(mapost,id);
-//    },
-//
-//    updatePost(entity){
-//        const id = entity.mapost;
-//        delete entity.mapost;
-//        return db('post').where(mapost,id).update(entity);
-//    }
-//
-//    deletePost(id){
-//        return db('post').where(mapost,id).del();
-//    }
-//}
+import db from '../utils/db.js'
+
+export default {
+
+    async createPost(entity,id1){
+         return await db.raw('call sp_ThemPost(?,?,?,?,?,?)',
+         entity.tenbh1,entity.casy1,entity.theloai1,id1,entity.loai1,entity.value1);
+    },
+
+    viewPostVideo(maPost){
+        return db(
+            db.raw('select * from post inner join post_video on post.mapost = post_video.mapost where post.mapost =' + maPost)
+        )
+    },
+
+    viewListPost(){
+    return db('post');
+    },
+
+    viewPostChord(maPost){
+        return db(
+            db.raw('select * from post inner join post_chord on post.mapost = post_chord.mapost where post.mapost =' + maPost)
+        )
+    },
+
+    delPost(maPost){
+        return db('post').where('mapost',mapost).del();
+    }
+
+}
