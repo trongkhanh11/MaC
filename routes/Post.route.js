77b2f8762223a6956633ef0fa1aa3668ddeef871
Su <sususu7702@gmail.com>
2023-01-04 00:23:22 +0700

    Su update fe

--- routes/Post.route.js
+++ routes/Post.route.js
@@ -7,19 +7,14 @@ router.get('/', function (req, res) {
   res.render('vwPost/Post');
 })
 
-<<<<<<< HEAD
 router.get('/Chords', async function (req, res) {
   res.render('vwPost/Chords');
 })
 
-=======
 router.post('/add', function (req, res) {
     console.log(req.body);
     res.redirect('../');
     return post_chordModel.createPost(req.body,2);
 })
 
-
-
->>>>>>> 21f1aff45c17f8bcb32a7430e4a0a3414fc76f3c
 export default router;
