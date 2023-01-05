6c61221d11674553bbe229ba0cfa2fc9607b07d9
Dat <datk1912@gmail.com>
2023-01-06 01:23:41 +0700

    Ghep code Playlist

--- routes/Playlist.route.js
+++ routes/Playlist.route.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import playlistModel from '../services/playlist.model.js';
 
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-  res.render('vwPlaylist/Playlist');
+    const list = await playlistModel.findAll();
+    console.log(list);
+    res.render('vwPlaylist/Playlist',{
+    playlist_list : list
+    });
 });
 
 router.get('/inplaylist', async function (req, res) {
-  res.render('vwPlaylist/inplaylist');
+    const pl = req.query.pl;
+    const playlist = await playlistModel.findOne(pl);
+    console.log(playlist);
+    res.render('vwPlaylist/inplaylist',{
+    playlist
+    });
 });
 
 router.get('/yourplaylist', async function (req, res) {
