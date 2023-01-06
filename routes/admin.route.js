cfd46ab23cbccab8e74e0c255b23b04d71fd1ae7
Dat <datk1912@gmail.com>
2023-01-06 20:37:44 +0700

    them admin route

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-  res.render('vwAdmin/index');
+  res.render('vwAdmin/index',{
+    layout: false
+  });
 });
 
 export default router;
